189e8ba48ff7889fbc4eab035dcb3c12714b442c
philwelsh <Phizs@hotmail.com>
2015-01-11 20:16:36 +0000

    first

---
+++ main.js
@@ -0,0 +1,34 @@
+﻿
+$(function () {
+    var currentpage
+    $(".menu li a").on("click", function () {
+        currentpage = $(this).attr("class");
+        $("body").removeClass("home about portfolio contact");
+        $("body").addClass(currentpage);
+        $(".stripe").removeClass("open close");
+        if (currentpage == "portfolio") {
+            $("body").css("position", "absolute");
+        }
+        else {
+            $("body").css("position", "fixed");
+        };
+    });
+
+    $(".project-button").on("click", function () {
+        $(this).parent().parent().addClass("open");
+        $(this).parent().parent().siblings().addClass("close");
+    });
+
+    //////////////TESTING////////////////
+    //$(".menu li a").hover(
+    //    function () {
+    //    alert(currentpage);
+    //    }
+    //);
+    //$('.freetile').freetilediv({
+    //    animate: true,
+    //    elementDelay: 30
+    //});
+
+
+});
