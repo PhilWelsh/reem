d67af6dbd6fe4e0516c9c9638a41ec0f771857af
philwelsh <Phizs@hotmail.com>
2015-01-24 15:32:42 +0000

    Extra pages added, functionality still required -image lightbox/carousel -funky about page layout

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 ﻿
 $(function () {
+
     var currentpage
+
+    //Click menu
     $(".menu li a").on("click", function () {
+
+        //change page to what was clicked
         currentpage = $(this).attr("class");
-        $("body").removeClass("home about portfolio contact");
+        $("body").removeClass("home about portfolio photography graphic-design life-drawing contact");
         $("body").addClass(currentpage);
+
+        //individual project page open
         $(".stripe").removeClass("open close");
-        if (currentpage == "portfolio") {
+
+        //make page scrollable if has content
+        if (currentpage !== "home" && currentpage !== "contact") {
             $("body").css("position", "absolute");
         }
         else {
@@ -15,8 +24,8 @@ $(function () {
     });
 
     $(".project-button").on("click", function () {
-        $(this).parent().parent().addClass("open");
-        $(this).parent().parent().siblings().addClass("close");
+        $(this).parent().parent().parent().addClass("open");
+        $(this).parent().parent().parent().siblings().addClass("close");
     });
 
     //////////////TESTING////////////////
